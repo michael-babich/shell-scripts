b0741ac182dd2e46584a2b28f43fec818ed91928
Michael Babich <72196425+michael-babich@users.noreply.github.com>
2023-01-25 15:43:04 +0200

    Update CheckVersions.bat

--- CheckVersions v1.0/CheckVersions.bat
+++ CheckVersions v1.0/CheckVersions.bat
@@ -1,1 +1,1 @@
-powershell -executionpolicy bypass -File .\CheckVersions.ps1 C:\Users\Michael\.m2\repository\org 4.5.12
+powershell -executionpolicy bypass -File .\CheckVersions.ps1 C:\Users\<user>\.m2\repository\org 4.5.12
